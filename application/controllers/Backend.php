f9dda9d45a75fdaebe5a4c8ae4f96dc6024e5606
DESKTOP-5UO351A\Miniball <winhackger@gmail.com>
2017-12-11 02:31:25 +0700

    asset dashbord backend

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -4,9 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Backend extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('layouts/header');
-		$this->load->view('layouts/navbar');
-		$this->load->view('backend/index');
-		$this->load->view('layouts/footer');
+		$this->load->view('layouts_backend/header');
+		$this->load->view('layouts_backend/backend_index_v');
 	}
 }
---
+++ application/views/layouts_backend/header.php
@@ -0,0 +1,57 @@
+<!-- Bootstrap CSS-->
+<link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.min.css">
+<!-- Fontastic Custom icon font-->
+<!-- <link rel="stylesheet" href="css/fontastic.css"> -->
+<!-- Font Awesome CSS-->
+<link rel="stylesheet" href="<?=base_url();?>assets/css/font-awesome.min.css">
+<!-- theme stylesheet-->
+<link rel="stylesheet" href="<?=base_url();?>assets/css/style.default.css" id="theme-stylesheet">
+<!-- Custom stylesheet - for your changes-->
+<link rel="stylesheet" href="<?=base_url();?>assets/css/custom.css">
+<!-- Javascript files-->
+<script src="<?=base_url();?>assets/js/jquery-3.2.1.js"></script>
+<script src="<?=base_url();?>assets/js/popper.min.js"> </script>
+<script src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
+<!-- <script src="vendor/jquery.cookie/jquery.cookie.js"> </script> -->
+<!-- <script src="vendor/jquery-validation/jquery.validate.min.js"></script> -->
+<!-- <script src="js/front.js"></script> -->
+<style>
+/* thai */
+    @font-face {
+        font-family: 'Itim';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Itim'), local('Itim-Regular'), url("<?=base_url();?>assets/fonts/Itim/K0fGzmj4WhCGfRyqF-GWtw.woff2") format('woff2');
+        unicode-range: U+0E01-0E5B, U+200B-200D, U+25CC;
+    }
+    /* vietnamese */
+    @font-face {
+        font-family: 'Itim';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Itim'), local('Itim-Regular'), url("<?=base_url();?>assets/fonts/Itim/1qW4J4UyzYGwACm7jNIVfA.woff2") format('woff2');
+        unicode-range: U+0102-0103, U+1EA0-1EF9, U+20AB;
+    }
+    /* latin-ext */
+    @font-face {
+        font-family: 'Itim';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Itim'), local('Itim-Regular'), url("<?=base_url();?>assets/fonts/Itim/cuu6XUTU27h0qA5IMLw1Mg.woff2") format('woff2');
+        unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+    }
+    /* latin */
+    @font-face {
+        font-family: 'Itim';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Itim'), local('Itim-Regular'), url("<?=base_url();?>assets/fonts/Itim/5IelbqBx4_S0xbIFnyjCdg.woff2") format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+    }
+    body {
+        font-family: 'Itim', cursive;
+        /* font-family: 'Kanit', sans-serif; */
+        font-size: 20px;
+    }
+</style>
+<div class="page home-page">
